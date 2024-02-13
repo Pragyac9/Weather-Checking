a8a66ace120492bbc4cdd9fec36e48448902e103
Pragyac9 <136442660+Pragyac9@users.noreply.github.com>
2024-02-13 14:57:54 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Weather from "./Components/WeatherApp/Weather"
+
+
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <Weather/>
+      </div>
+    </>
+  )
+}
+
+export default App
